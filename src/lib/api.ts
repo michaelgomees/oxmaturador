6a3b5736e59601df45f8fe3da16729ac41df58a2
michaelgomees <michaelgomescel@gmail.com>
2025-09-06 22:58:11 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.oxautomacoes.com.br',
+  baseURL: 'https://ws.oxautomacoes.com.br', // <<-- URL CORRIGIDA
   headers: {
     'Content-Type': 'application/json',
   },
